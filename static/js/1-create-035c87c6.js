e59eaac3d030287384f4f9ef8ce34d4fa195fa95
小黎 <[22708206@qq.com]>
2023-02-02 18:04:57 +0800

    Updates

---
+++ static/js/1-create-035c87c6.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as r}from"./index-956a789d.js";const o={};function t(s,_){return c(),r("div",null,"\u521B\u5EFA")}const n=e(o,[["render",t],["__file","D:/Demo/\u521B\u610F/fabricjs-demo/src/views/demo/1-create.vue"]]);export{n as default};
---
+++ static/js/2-background-4f060911.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o}from"./index-956a789d.js";const r={};function n(s,_){return c(),o("div",null,"\u80CC\u666F")}const t=e(r,[["render",n],["__file","D:/Demo/\u521B\u610F/fabricjs-demo/src/views/demo/2-background.vue"]]);export{t as default};
